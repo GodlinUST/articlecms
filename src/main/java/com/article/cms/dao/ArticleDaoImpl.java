32c1dca9c7016e1d78c792ae625f0ffb2f28afc5
jayant kumar <jaykrs@gmail.com>
2016-05-13 00:10:22 +0530

    new code

---
+++ src/main/java/com/article/cms/dao/ArticleDaoImpl.java
@@ -0,0 +1,42 @@
+package com.article.cms.dao;
+
+import java.util.List;
+
+
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Repository;
+
+import com.article.cms.model.Article;
+
+@Repository(value = "articleDao")
+public class ArticleDaoImpl extends AbstractDao<Integer, Article> implements ArticleDao{
+
+	
+	@SuppressWarnings("unchecked")
+	public List<Article> listArticles() {
+		List<Article> list;
+		list = getSession().createCriteria(Article.class).list();
+				return list;
+	}
+
+	public Article getArticle(int id) {
+		return (Article) getSession().createCriteria(Article.class).add(Restrictions.eq("id", new Long(id))).uniqueResult();
+		
+	}
+
+	public void addArticle(Article article) {
+		getSession().persist(article);
+	}
+
+	public void updateArticle(Article article) {
+		getSession().saveOrUpdate(article);
+	}
+
+	public boolean deleteArticle(int id) {
+		boolean t = false;
+		getArticle(id);
+		getSession().delete(getArticle(id));
+		return t;
+	}
+
+}
